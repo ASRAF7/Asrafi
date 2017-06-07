f1e840ab000844d29ea25b7029899c9bd237e460
ASRAF7 <asraf.aliece@ksrce.ac.in>
2017-06-07 12:10:39 +0530

    Add files via upload

---
+++ afab.java
@@ -0,0 +1,12 @@
+public class java{
+public static void main(String []asgs){
+int n1=0,n2=1,n3,i,cout=10;
+System.out.println(n1+" "+n2);
+for(i=2;i<cout;i++)
+{
+n3=n2+n1;
+System.out.println(" "+n3);
+n1=n2;
+n2=n3;
+}
+}}
