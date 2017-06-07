4373bbd9f2f3d6389b0c6a86d6df379fc434ba79
ASRAF7 <asraf.aliece@ksrce.ac.in>
2017-06-07 12:23:21 +0530

    Add files via upload

---
+++ aswap.java
@@ -0,0 +1,16 @@
+public class{
+import java.util.Scanner;
+public static void main(String [] arg{
+int a,b,c;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the 1st value a");
+a=sc.next();
+System.out.println("enter the 1st value b");
+b=sc.next();
+c=a;
+a=b;
+b=c;
+System.out.println("enter the 1st value a",+a);
+System.out.println("enter the 2nd value b",+b);
+}
+}
