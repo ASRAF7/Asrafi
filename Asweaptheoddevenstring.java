396919faeec0f257eedd4d92b7a6ef9d2340f9ba
ASRAF7 <asraf.aliece@ksrce.ac.in>
2017-07-04 11:38:29 +0530

    Add files via upload

---
+++ Asweaptheoddevenstring.java
@@ -0,0 +1,17 @@
+import java.util.*;
+@SuppressWarnings("unused")
+public class Asweaptheoddevenstring {
+public static void main(String[] args) {
+String s1="asraf";
+for(int i=0;i<s1.length();i++){
+	if(s1.charAt(i)%2==0){
+		System.out.println ("the even char of String "+ s1.charAt(i));}
+}for(int j=0;j<s1.length();j++){
+	if(s1.charAt(j)%2!=0){
+		System.out.println("the odd char of string "+ s1.charAt(j));
+	}
+}
+
+
+}
+}
