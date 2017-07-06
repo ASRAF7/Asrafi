77c450c7a39370c2677c3aa3b473c39d99ab3002
ASRAF7 <asraf.aliece@ksrce.ac.in>
2017-07-06 14:04:07 +0530

    Add files via upload

---
+++ Abg3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Abg3 {
+@SuppressWarnings("resource")
+public static void main(String[] args) {
+
+    Scanner in=new Scanner(System.in);
+    System.out.println("Enter the number : ");
+    int num = in.nextInt();
+
+    @SuppressWarnings("unused")
+	int other = 1;  
+    if(((~num) & 1) == 1)  
+    {  
+        System.out.println("The number is a power of two");  
+    }  
+    else  
+    {
+        System.out.println("The number is a  NOT A power of two");  
+    }
+
+
+}
+}
