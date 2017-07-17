1e1b9e5181705933d181ca246c58714a2f7cfaa8
ASRAF7 <asraf.aliece@ksrce.ac.in>
2017-07-17 14:36:12 +0530

    Add files via upload

---
+++ coprime.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+int hcf(int a, int h)
+{
+    int temp;
+    while(1)
+    {
+        temp = a%h;
+        if(temp==0)
+        return h;
+        a = h;
+        h = temp;
+    }
+}
+
+int main()
+{
+    int c,d,gcd;
+    printf("enter two values of co prime\n");
+    scanf("%d%d",&c,&d);
+    gcd=hcf(c,d);
+
+    if(gcd==1)
+    {
+        printf("\n%d and %d are  co prime numbers .",c,d);
+    }
+    else
+    {
+        printf("\n%d and %d are not  co prime numbers .",c,d);
+    }
+    getch();
+}
